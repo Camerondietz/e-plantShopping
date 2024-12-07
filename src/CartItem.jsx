fc92c6888a12a5ebf88e829592b2803f99dbc2b2
Cameron Dietz <dietz.cameron@gmail.com>
2024-12-07 00:11:15 -0600

    Add files via upload

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,44 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+      //return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+      let Total = 0;
+      cart.forEach(item => {
+          Total += item.quantity * parseFloat(item.cost.substring(1));
+      });
+      return Total;
   };
 
-  const handleContinueShopping = (e) => {
+    const handleContinueShopping = (e) => {
+        onContinueShopping(e);
+      //e.preventDefault();
+      //setShowCart(false);
+  };
+
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+  };
+
+    const handleIncrement = (item) => {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }))
+  };
+
+    const handleDecrement = (item) => {
+        if (item.quantity > 1) {
+            dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }))
+        } else { dispatch(removeItem(item.name)) }
    
   };
 
-
-
-  const handleIncrement = (item) => {
-  };
-
-  const handleDecrement = (item) => {
-   
-  };
-
-  const handleRemove = (item) => {
+    const handleRemove = (item) => {
+        dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
+    const calculateTotalCost = (item) => {
+        //cart.find(item => item.name === item).quantity * ;
+        //return cart[item].quantity * cart[item].quantity;
+        return item.quantity * parseFloat(item.cost.substring(1));
   };
 
   return (
